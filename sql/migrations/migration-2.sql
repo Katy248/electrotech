fafd930aa2daac1eba9176debd7fc39700e9c076
Katy248 <petrovanton247@gmail.com>
2025-12-07 23:31:06 +0300

    Fix migration

--- sql/migrations/migration-2.sql
+++ sql/migrations/migration-2.sql
@@ -3,7 +3,7 @@ ALTER TABLE order_products
 DROP COLUMN quantity;
 
 ALTER TABLE order_products
-ADD COLUMN quantity REAL NOT NULL;
+ADD COLUMN quantity REAL NOT NULL DEFAULT 1;
 
 -- +migrate Down
 ALTER TABLE order_products
