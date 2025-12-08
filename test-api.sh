83b9d63a9656f04e04a44867186b77421819ee55
Katy248 <petrovanton247@gmail.com>
2025-12-08 11:50:14 +0300

    Clean files

--- test-api.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -X POST localhost:1488/api/user/register -d '{"email": "petrovanton247@gmail.com", "password": "password", "first_name" :"Anton", "surname":"Petrov", "last_name" :"V", "phone_number":"123"}' | jq
-
-curl -X POST localhost:1488/api/user/login -d '{"email": "petrovanton247@gmail.com", "password": "password"}' | jq
